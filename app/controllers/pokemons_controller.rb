7d41dce8cb11a0a93c96f673299f54ed686a4391
Alexander Schelchere <ascellie@gmail.com>
2019-06-28 14:05:45 -0400

    link to one pokemon

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,25 @@
 class PokemonsController < ApplicationController
 
     def index
-        @pokemons = Pokemon.all
+        @pokemons = poke_all
     end
+
+    def show
+        @pokemon = poke_one
+    end
+
+
+
+
+    private
+    # All pokemon
+    def poke_all
+        Pokemon.all
+    end
+
+    # one pokemon
+    def poke_one
+        poke_all.find(params[:id])
+    end
+
 end
