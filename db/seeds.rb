c70126a538a664cf7a373540ec6683525a70af6e
Alexander Schelchere <ascellie@gmail.com>
2019-06-28 13:16:02 -0400

    rails resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+all_pokemon = Pokemon.create(
+    [
+        {
+            name: 'Pikachu',
+            weight: 60,
+            type: 'electric',
+            description: 'Pikachu is a mouse pokemon who can discharge electricity from the cheeks.'
+        },
+        {
+            name: 'Quilava',
+            weight: 190,
+            type: 'fire',
+            description: 'Quilava is a quadruped Pokémon with a long, slender body.'
+        },
+        {
+            name: 'Psyduck',
+            weight: 196,
+            type: 'water',
+            description: 'Psyduck is a yellow Pokémon resembling a duck or bipedal platypus.'
+        },
+        {
+            name: 'Machop',
+            weight: 195,
+            type: 'fighting',
+            description: 'Machop is a humanoid, bipedal Pokémon that has gray skin.'
+        },
+        {
+            name: 'Mew',
+            weight: 40,
+            type: 'psychic',
+            description: 'Mew is a Mythical Pokémon introduced in Generation I. It is not known to evolve into or from any other Pokémon.'
+        },
+        {
+            name: 'Snorlax',
+            weight: 4600,
+            type: 'normal',
+            description: 'Snorlax is often found in mountains and forests. It wakes up only to eat, requiring 900 lbs. (400 kg) of food per day before returning to its slumber.'
+        },
+        {
+            name: 'Dragonite',
+            weight: 2100,
+            type: 'dragon, flying',
+            description: 'Dragonite is capable of circling the globe in just 16 hours. It is a kindhearted Pokémon that leads lost and foundering ships in a storm to the safety of land.'
+        }
+    ]
+)
